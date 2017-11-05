8150cbd31e921b020cc6cc63cad74425b2ab7ac8
mac craig <maccraig98@gmail.com>
2017-11-05 00:28:06 -0700

    Off by one argv

--- gps.py
+++ gps.py
@@ -51,7 +51,7 @@ import time
 # Speed at which to refresh the program
 # First command line argument
 if len(sys.argv) > 0:
-    speed = int(sys.argv[1])
+    speed = int(sys.argv[0])
 else:
     speed = 0
 
